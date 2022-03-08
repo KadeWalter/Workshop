39e49202fb20a7212bbd1bcaa02c61601ec2aa23
Kade Walter <kadewalter1@gmail.com>
2022-03-08 14:24:13 -0600

    duplicate completed project

---
+++ NSDiffableDataSource/IntroWithTableView/BasicNSDiffableDataSourceExampleCompleted/BasicNSDiffableDataSourceExample/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  BasicNSDiffableDataSourceExample
+//
+//  Created by Kade Walter on 3/3/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: scene)
+        
+        let navCont = UINavigationController(rootViewController: ViewController())
+        navCont.navigationBar.prefersLargeTitles = false
+        
+        window.rootViewController = navCont
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
